77ef4053aea7498e6c3b7969385ea2b106cbc0a1
minimoneym0 <90237217+minimoneym0@users.noreply.github.com>
2024-09-18 11:07:21 +0300

    Update sitemap-script.php with add Name
    add name in xml result

--- script.php
+++ script.php
@@ -7,14 +7,16 @@ $urlset = $dom->createElement("urlset");
 //$urlset->setAttributeNS('http://www.w3.org/2000/xmlns/','xmlns:image','http://www.google.com/schemas/sitemap-image/1.1');
 
 $arSelect = Array("ID", "NAME", "DETAIL_PAGE_URL", "DETAIL_PICTURE");
-$arFilter = Array("IBLOCK_ID"=>1, "SECTION_ID"=>$arResult['ID'], "INCLUDE_SUBSECTIONS" => "Y"); //ID Инфоблока и ID раздела с элементами
-$rsElement = CIBlockElement::GetList(Array("NAME" => "ASC"), $arFilter, false, Array("nPageSize"=>20), $arSelect);
+$arFilter = Array("IBLOCK_ID"=>34, "SECTION_ID"=>$arResult['ID'], "INCLUDE_SUBSECTIONS" => "Y"); //ID Инфоблока и ID раздела с элементами
+$rsElement = CIBlockElement::GetList(Array("NAME" => "ASC"), $arFilter, false, Array("nPageSize"=>7500), $arSelect);
 $arResult["ITEMS"] = array();
 while($obElement = $rsElement->GetNextElement())
 {
 $arItem = $obElement->GetFields();
 $arItem["PROPERTIES"] = $obElement->GetProperties();
-$google_link =  'https://site.ru'.$arItem[DETAIL_PAGE_URL];
+$google_link =  'https://shop.cheaz.ru'.$arItem[DETAIL_PAGE_URL];
+
+$fin_name = $arItem[NAME];
 
 $timezone = date("Y-m-d H:i:s");
 $pr = '0.7';
@@ -22,6 +24,9 @@ $pr = '0.7';
     $url = $dom->createElement("url"); 
     $loc = $dom->createElement("loc", $google_link);
     $url->appendChild($loc);
+
+    $name = $dom->createElement("name", $fin_name);
+    $url->appendChild($name);
 
     $lastmod = $dom->createElement("lastmod", $timezone);
     $url->appendChild($lastmod);
